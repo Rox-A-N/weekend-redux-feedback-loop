ee0defa979de03a3e5faed559c62936cd23f6c8c
Roxanne Alvidrez Niederkorn <rox.niederkorn@gmail.com>
2023-02-04 19:17:15 -0600

    Formatted first part: How are you feeling; CSS for the NEXT button

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -10,6 +10,13 @@ function App() {
         <h1 className='App-title'>Feedback!</h1>
         <h4>Don't forget it!</h4>
       </header>
+    <div>
+      <h2>How are you feeling today?</h2>
+      <p>(Where 5 = Great! and 0 = Unsure)</p>
+      <label htmlFor="feeling">Feeling?</label>
+      <input type="number" min={0} max={5}/>
+      <button className='button'>NEXT</button>
+    </div>
     </div>
   );
 }
