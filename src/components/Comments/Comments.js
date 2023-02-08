4d49c2fb1a1241e2cac55db548a0d67f84f9c3b3
Roxanne Alvidrez Niederkorn <rox.niederkorn@gmail.com>
2023-02-08 00:18:50 -0600

    Updated onClick function

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -1,13 +1,36 @@
 // form for comments // change wording
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 
 
 // form for adding comments
 function Comments() {
+
   const history = useHistory();
+  const dispatch = useDispatch();
+  const [comment, setComment] = useState( '' );
+
+  const commentsReducer = useSelector((store) => store.commentsReducer)
+
+  const handleChange = (event) => {
+    dispatch({
+        type: 'SET_COMMENT',
+        payload: comment 
+    });
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('comments are submitted?', comment);
+
+    handleChange();
+    routeToFeedback();
+
+  }
+
+
   const routeToFeedback = () => {
-    // alert("woo! feelings!!");
     history.push('/feedback');
   }
   
@@ -16,9 +39,11 @@ function Comments() {
 
       <h2>Any comments you want to leave?</h2>
       
-      <label htmlFor="comments">Comments:</label><br/>
-      <textarea id="comments" rows="4" cols="50"></textarea>
-      <button className='button' onClick={ routeToFeedback }>NEXT</button>
+      <form>
+      <label>Comments:</label><br/>
+        <textarea id="comments" rows="4" cols="50" value={comment} onChange={(event) => setComment(event.target.value)}></textarea>
+        <button className='button' onClick={ handleSubmit }>NEXT</button>
+      </form>
     
     </div>
   );
